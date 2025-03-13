a97a7a3bbc84745be60b76760846f0b170727eb1
katie-060330 <kate.lonsway@outlook.com>
2025-03-13 13:26:08 -0400

    adding test data

--- src/main/java/code/Connectiondb.java
+++ src/main/java/code/Connectiondb.java
@@ -1,32 +1,45 @@
 package code;
+
 import java.sql.*;
 
 public class Connectiondb {
     // Database connection parameters
     public static void main(String[] args) {
-        try(Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/WebApp","postgres","new_password");){
+        try (Connection db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/WebApp", "postgres", "new_password");) {
             System.out.println("Connected");
 
             Statement stmt = db.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
-            while(rs.next()){
+            ResultSet rs = stmt.executeQuery("SELECT * FROM employee");
+            while (rs.next()) {
 
-                    System.out.print(rs.getString(3) + ", ");
+                //getting the test data
+                String employeeID = rs.getString("employeeID");
+                String sin = rs.getString("sin");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                String middleInitials = rs.getString("middleInitials");
+                String name = rs.getString("name");
+                String address = rs.getString("address");
+                String role = rs.getString("role");
+
+                //prinign out the line
+                System.out.println(
+                        "EmployeeID: " + employeeID +
+                        ", SIN: " + sin +
+                        ", First Name: " + firstName +
+                        ", Last Name: " + lastName +
+                        ", Middle Initials: " + middleInitials +
+                        ", Name: " + name +
+                        ", Address: " + address +
+                        ", Role: " + role);
 
             }
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
 
 
-
-        
-
-        
     }
 
-    
-    
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<p>THis is the first web app</p>
+<p>This is the first web app</p>
+
 </body>
 </html>
